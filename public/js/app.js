1d2a242919bac435ed8880e9d6bacacaec2f1d3c
devncu <46550214+devncu@users.noreply.github.com>
2020-07-23 17:19:31 +0530

    setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -14,7 +14,7 @@ messagetwo.textContent=''
 
     const location = search.value
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                messageone.textContent=data.error
